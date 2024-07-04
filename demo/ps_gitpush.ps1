6f2c9591340ea7231a1f6c98bfa3fb8585f42bc7
unknown <Ha Local-Win>
2024-07-04 16:59:12 +0700

    update at 2024-07-04_16-59-08 from PC

--- demo/ps_gitpush.ps1
+++ demo/ps_gitpush.ps1
@@ -2,4 +2,4 @@ $currentDateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
 git pull origin
 git add E:\Documents\project\ansible\
 git commit -m "update at $currentDateTime from PC"
-git push origin
+git push github
