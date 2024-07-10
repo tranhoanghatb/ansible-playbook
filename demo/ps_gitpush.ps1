c112819f9865b4b1745f7b411ad81e517ef0745a
Tran Hoang Ha <hath@onepay.local>
2024-07-10 14:39:00 +0700

    update at  from PC

--- demo/ps_gitpush.ps1
+++ demo/ps_gitpush.ps1
@@ -1,5 +1,5 @@
 $currentDateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
 git pull origin
-git add E:\Documents\project\ansible\
+git add .
 git commit -m "update at $currentDateTime from PC"
-git push github
+git push origin
